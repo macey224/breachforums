f1d2ba55f589145a6d99170f3babc6ea8c8faf52
macey224 <106244270+macey224@users.noreply.github.com>
2023-12-14 09:21:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ $(document).ready( function() {
         iDisplayLength: -1
     });
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    function addDigitSeparators() {
+        var table = document.getElementById('data-table');
+        var rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+
+        for (var i = 0; i < rows.length; i++) {
+            var cell = rows[i].getElementsByTagName('td')[1];
+            var number = parseInt(cell.textContent.replace(/,/g, ''), 10);
+            cell.textContent = number.toLocaleString();
+        }
+    }
+    addDigitSeparators();
+});
